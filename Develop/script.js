fb7deacb2ba1494ebd2c5933eb2ebddf83894b82
shuib <shuaib_97@hotmail.co.uk>
2022-10-14 21:13:47 +0100

    Creating a function

---
+++ Develop/script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+  window.prompt ('hello world')
+}
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
